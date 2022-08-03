10e5b4605d2788b01b4f4e81f174e769821e4a93
gnurts <ggnnuurrtt@gmail.com>
2022-08-03 09:34:22 +0700

    mofify

--- src/components/Text.js
+++ src/components/Text.js
@@ -3,11 +3,14 @@ import { Text as RNText } from 'react-native'
 import { isString } from '../utils/checkType'
 import styles from '../assets/styles/text.styles'
 
-const Text = ({ children, style }) => {
+const Text = ({ children, style, numberOfLines }) => {
     if(!isString(children)) throw new Error('Children must be a string') 
     
     return (
-        <RNText style={[styles, {...style}]}>
+        <RNText
+            style={[styles, {...style}]}
+            numberOfLines={numberOfLines}
+        >
             {children}
         </RNText>
     )
