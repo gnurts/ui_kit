ee6b3edc33eb329f5f4f21d5cd63f0df040f1b0f
gnurts <ggnnuurrtt@gmail.com>
2022-08-26 23:47:05 +0700

    modify

--- src/components/Text.js
+++ src/components/Text.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import { Text as RNText } from 'react-native'
-import { isString } from '../utils/checkType'
+import { isString, isNull } from '../utils/checkType'
 import styles from '../assets/styles/text.styles'
 
-const Text = ({ children, style, numberOfLines }) => {
-    if(!isString(children)) throw new Error('Children must be a string') 
+const Text = ({
+    children = '',
+    style,
+    numberOfLines
+}) => {
+    if(isNull(children)) children = ''
+    if(!isString(children)) children = children.toString()
     
     return (
         <RNText
